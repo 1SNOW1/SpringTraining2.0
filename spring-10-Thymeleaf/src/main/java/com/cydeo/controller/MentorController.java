25af7c8cda99d923a0cc44ff46a269a7606a6129
enesorhan <enestcp@gmail.com>
2024-06-20 09:56:25 +0300

    Table creation practice, and some repositories

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -5,6 +5,7 @@ import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -25,6 +26,20 @@ public class MentorController {
 
 
         return "/mentor/mentor-register";
+    }
+
+
+    @PostMapping("/confirm")
+    public String submitForm1(){
+
+        return "mentor/mentor-confirmation";
+
+    }
+    @GetMapping("/confirm")
+    public String submitForm2(){
+
+        return "mentor/mentor-confirmation";
+
     }
 
 }
